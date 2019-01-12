0783e387684eb102246c4db2cb763c0c6f20e916
krister <krister@sent.at>
2019-01-12 11:03:17 -0300

    Add more loop tests

--- e2e/basic.spec.js
+++ e2e/basic.spec.js
@@ -54,7 +54,7 @@ describe("Basic carousel example", () => {
   });
 
   describe("loop", () => {
-    it("should be able to go until the end and back to the first page", async () => {
+    it("should be able to go to Page 1 -> Page 2 -> Page 3 -> Page 1", async () => {
       await expect(element(by.id("loop"))).toBeVisible();
       await element(by.id("loop")).tap();
 
@@ -75,6 +75,75 @@ describe("Basic carousel example", () => {
       await expect(element(by.id("PinarNextButton"))).toBeVisible();
 
       await element(by.id("PinarNextButton")).tap();
+
+      // Page 3
+      await expect(element(by.id("slide-1"))).toBeNotVisible();
+      await expect(element(by.id("slide-2"))).toBeNotVisible();
+      await expect(element(by.id("slide-3"))).toBeVisible();
+      await expect(element(by.id("PinarPrevButton"))).toBeVisible();
+      await expect(element(by.id("PinarNextButton"))).toBeVisible();
+
+      await element(by.id("PinarNextButton")).tap();
+
+      // Page 1
+      await expect(element(by.id("slide-1"))).toBeVisible();
+      await expect(element(by.id("slide-2"))).toBeNotVisible();
+      await expect(element(by.id("slide-3"))).toBeNotVisible();
+      await expect(element(by.id("PinarPrevButton"))).toBeVisible();
+      await expect(element(by.id("PinarNextButton"))).toBeVisible();
+    });
+
+    it("should be able to go to Page 1 -> Page 3 -> Page 2 -> Page 1", async () => {
+      await expect(element(by.id("loop"))).toBeVisible();
+      await element(by.id("loop")).tap();
+
+      // Page 1
+      await expect(element(by.id("slide-1"))).toBeVisible();
+      await expect(element(by.id("slide-2"))).toBeNotVisible();
+      await expect(element(by.id("slide-3"))).toBeNotVisible();
+      await expect(element(by.id("PinarPrevButton"))).toBeVisible();
+      await expect(element(by.id("PinarNextButton"))).toBeVisible();
+
+      await element(by.id("PinarPrevButton")).tap();
+
+      // Page 3
+      await expect(element(by.id("slide-1"))).toBeNotVisible();
+      await expect(element(by.id("slide-2"))).toBeNotVisible();
+      await expect(element(by.id("slide-3"))).toBeVisible();
+      await expect(element(by.id("PinarPrevButton"))).toBeVisible();
+      await expect(element(by.id("PinarNextButton"))).toBeVisible();
+
+      await element(by.id("PinarPrevButton")).tap();
+
+      // Page 2
+      await expect(element(by.id("slide-1"))).toBeNotVisible();
+      await expect(element(by.id("slide-2"))).toBeVisible();
+      await expect(element(by.id("slide-3"))).toBeNotVisible();
+      await expect(element(by.id("PinarPrevButton"))).toBeVisible();
+      await expect(element(by.id("PinarNextButton"))).toBeVisible();
+
+      await element(by.id("PinarPrevButton")).tap();
+
+      // Page 1
+      await expect(element(by.id("slide-1"))).toBeVisible();
+      await expect(element(by.id("slide-2"))).toBeNotVisible();
+      await expect(element(by.id("slide-3"))).toBeNotVisible();
+      await expect(element(by.id("PinarPrevButton"))).toBeVisible();
+      await expect(element(by.id("PinarNextButton"))).toBeVisible();
+    });
+
+    it("should be able to go to Page 1 -> Page 3 -> Page 1", async () => {
+      await expect(element(by.id("loop"))).toBeVisible();
+      await element(by.id("loop")).tap();
+
+      // Page 1
+      await expect(element(by.id("slide-1"))).toBeVisible();
+      await expect(element(by.id("slide-2"))).toBeNotVisible();
+      await expect(element(by.id("slide-3"))).toBeNotVisible();
+      await expect(element(by.id("PinarPrevButton"))).toBeVisible();
+      await expect(element(by.id("PinarNextButton"))).toBeVisible();
+
+      await element(by.id("PinarPrevButton")).tap();
 
       // Page 3
       await expect(element(by.id("slide-1"))).toBeNotVisible();
