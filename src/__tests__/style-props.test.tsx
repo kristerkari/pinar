9626a340511f66b6d2ba98b0291ce5d1fceb80f8
krister <krister@sent.at>
2019-01-13 17:01:45 -0300

    Add more unit tests for custom styles

--- src/__tests__/style-props.test.tsx
+++ src/__tests__/style-props.test.tsx
@@ -18,6 +18,40 @@ describe("style props", () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  it("should allow custom controlsButtonStyle", () => {
+    const wrapper = shallow(
+      <Carousel controlsButtonStyle={{ backgroundColor: "blue" }} index={1}>
+        <View>
+          <Text>1</Text>
+        </View>
+        <View>
+          <Text>2</Text>
+        </View>
+        <View>
+          <Text>3</Text>
+        </View>
+      </Carousel>
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should allow custom controlsTextStyle", () => {
+    const wrapper = shallow(
+      <Carousel controlsTextStyle={{ color: "blue" }} index={1}>
+        <View>
+          <Text>1</Text>
+        </View>
+        <View>
+          <Text>2</Text>
+        </View>
+        <View>
+          <Text>3</Text>
+        </View>
+      </Carousel>
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
   it("should allow custom dotsContainerStyle", () => {
     const wrapper = shallow(
       <Carousel dotsContainerStyle={{ backgroundColor: "blue" }}>
