74b633b03208e4e2b933c71dc39a913dc0391a66
krister <krister@sent.at>
2020-03-18 19:42:19 +0200

    Update React Native and Detox to latest versions

---
+++ examples/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: '../.eslintrc',
+  rules: {
+    'react/jsx-no-bind': 0,
+    'react/jsx-sort-props': [0],
+  },
+};
--- examples/metro.config.js
+++ examples/metro.config.js
@@ -1,8 +1,22 @@
-const path = require("path");
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 
+const path = require("path");
 const blacklist = require("metro-config/src/defaults/blacklist");
 
 module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false
+      }
+    })
+  },
   resolver: {
     blacklistRE: blacklist([/\/pinar\/node_modules\/react-native\/.*/]),
     extraNodeModules: {
