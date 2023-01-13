008bcad9cfc96ce79f7d2d84c3f1dfe1b59f72e0
Krister Kari <krister@sent.at>
2023-01-13 20:08:55 -0300

    Update React Native to 0.71

--- examples/metro.config.js
+++ examples/metro.config.js
@@ -6,7 +6,7 @@
  */
 
 const path = require("path");
-const exclusionList = require('metro-config/src/defaults/exclusionList');
+const exclusionList = require("metro-config/src/defaults/exclusionList");
 
 module.exports = {
   transformer: {
@@ -14,15 +14,15 @@ module.exports = {
       transform: {
         experimentalImportSupport: false,
         inlineRequires: true,
-      }
-    })
+      },
+    }),
   },
   resolver: {
     blacklistRE: exclusionList([/\/pinar\/node_modules\/react-native\/.*/]),
     extraNodeModules: {
       react: path.resolve(__dirname, "node_modules/react"),
-      "react-native": path.resolve(__dirname, "node_modules/react-native")
-    }
+      "react-native": path.resolve(__dirname, "node_modules/react-native"),
+    },
   },
   /**
    * Add our workspace roots so that react native can find the source code for the included packages
@@ -31,6 +31,6 @@ module.exports = {
   projectRoot: path.resolve(__dirname),
   watchFolders: [
     path.resolve(__dirname, "../src"),
-    path.resolve(__dirname, "../node_modules")
-  ]
+    path.resolve(__dirname, "../node_modules"),
+  ],
 };
