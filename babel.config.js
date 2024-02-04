c11a298e5c2a6214a17c02022b95e100a944c41f
Krister Kari <musky_heating0p@icloud.com>
2024-02-04 20:22:34 -0300

    Update React Native to 0.73

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-  plugins: [
-    "@babel/plugin-proposal-private-methods",    
-    '@babel/plugin-proposal-class-properties'
-  ],
+  presets: ["module:@react-native/babel-preset"],
 };
