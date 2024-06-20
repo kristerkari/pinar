5a4fbe8338a4f71decab6e9d20cd49199ab59e34
Krister Kari <musky_heating0p@icloud.com>
2024-06-20 13:55:54 +0300

    Update React Native to 0.74.2

--- examples/metro.config.js
+++ examples/metro.config.js
@@ -1,15 +1,15 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 const path = require("path");
 const exclusionList = require("metro-config/src/defaults/exclusionList");
 
 /**
  * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
+ * https://reactnative.dev/docs/metro
  *
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
-    resolver: {
+  resolver: {
     blacklistRE: exclusionList([/\/pinar\/node_modules\/react-native\/.*/]),
     extraNodeModules: {
       react: path.resolve(__dirname, "node_modules/react"),
